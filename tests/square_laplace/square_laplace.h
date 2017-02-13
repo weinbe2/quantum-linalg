c9b4a21f2704f8b2249becf868ac94b89ed6f627
Evan Weinberg <weinbe2@bu.edu>
2017-02-13 16:51:14 -0500

    Added a complex laplace to test complex functions.

--- tests/square_laplace/square_laplace.h
+++ tests/square_laplace/square_laplace.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2017 Evan S Weinberg
-// Test header for a real operator.
+// Test header for a real and complex operator.
 
 #ifndef SQUARE_LAPLACE_HEADER
 #define SQUARE_LAPLACE_HEADER
@@ -8,6 +8,13 @@ struct laplace_struct
 {
   int length;
   double m_sq;
+};
+
+struct laplace_gauged_struct
+{
+  int length;
+  double m_sq;
+  std::complex<double>* gauge_links; // size 2*length*length
 };
 
 #endif // SQUARE_LAPLACE_HEADER
