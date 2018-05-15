33e23f06df722c467330d24b443f08d937df069d
Evan Weinberg <evansweinberg@gmail.com>
2018-05-15 00:40:12 -0400

    Added another matrix-vector function.

--- blas/generic_matrix.h
+++ blas/generic_matrix.h
@@ -188,6 +188,87 @@ template<typename T> inline void cMATxy(T* __restrict__ mat, T* __restrict__ x, 
   }
 }
 
+template<typename T> inline void cMAT_single_xy(T* __restrict__ mat, T* __restrict__ x, T* __restrict__ y, const int nelem, const int nrow, const int ncol)
+{
+  if (nrow == 1 && ncol == 1)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<1,1>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 2 && ncol == 2)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<2,2>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 4 && ncol == 4)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<4,4>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 8 && ncol == 8)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<8,8>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 10 && ncol == 10)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxpy_local<10,10>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 12 && ncol == 12)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<12,12>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 16 && ncol == 16)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<16,16>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 20 && ncol == 20)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<20,20>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 24 && ncol == 24)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<24,24>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else if (nrow == 32 && ncol == 32)
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local<32,32>(mat, x+i*ncol, y+i*nrow);
+    }
+  }
+  else
+  {
+    for (int i = 0; i < nelem; i++)
+    {
+      cMATxy_local(mat, x+i*ncol, y+i*nrow, nrow, ncol);
+    }
+  }
+}
+
 #else
 
 // Apply the global mat-vec operation in row-major.
@@ -206,6 +287,14 @@ template<typename T> inline void cMATxy(T* __restrict__ mat, T* __restrict__ x, 
   for (int i = 0; i < nelem; i++)
   {
     cMATxy_local(mat+i*mat_vol, x+i*ncol, y+i*nrow, nrow, ncol);
+  }
+}
+
+template<typename T> inline void cMAT_single_xy(T* __restrict__ mat, T* __restrict__ x, T* __restrict__ y, const int nelem, const int nrow, const int ncol)
+{
+  for (int i = 0; i < nelem; i++)
+  {
+    cMATxy_local(mat, x+i*ncol, y+i*nrow, nrow, ncol);
   }
 }
 
@@ -262,6 +351,28 @@ template<typename T> inline void cMATxtMATyMATz_square(T* xmat, T* ymat, T* zmat
   }
 }
 
+// Do a mat-mat operation in row-major.
+// Z = X*Y, square matrix, X same everywhere
+template<typename T> inline void cMATx_single_tMATyMATz_square(T* xmat, T* ymat, T* zmat, int nelem, int ndim)
+{
+  const int mat_vol = ndim*ndim;
+  for (int i = 0; i < nelem; i++)
+  {
+    cMATxtMATyMATz_square_local(xmat, ymat+i*mat_vol, zmat+i*mat_vol, ndim);
+  }
+}
+
+// Do a mat-mat operation in row-major.
+// Z = X*Y, square matrix, Y same everywhere
+template<typename T> inline void cMATxtMATy_single_MATz_square(T* xmat, T* ymat, T* zmat, int nelem, int ndim)
+{
+  const int mat_vol = ndim*ndim;
+  for (int i = 0; i < nelem; i++)
+  {
+    cMATxtMATyMATz_square_local(xmat+i*mat_vol, ymat, zmat+i*mat_vol, ndim);
+  }
+}
+
 // Perform a QR decomposition in row-major.
 // Complex only.
 // xmat -> qmat * rmat
@@ -299,3 +410,4 @@ template<typename T> inline void cMATqr_do_det_square(complex<T>* qmat, complex<
 
 #endif // QLINALG_MATRIX
 
+// Compute constant mat times vec, repeating the same mat.
