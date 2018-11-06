ff0c1400f4da09e5b366b06d3ae546d059fb30f3
Evan Weinberg <evansweinberg@gmail.com>
2018-11-06 10:05:24 -0500

    Added CA-CG.

---
+++ inverters/generic_ca_cg.h
@@ -0,0 +1,499 @@
+// Copyright (c) 2018 Evan S Weinberg
+// CG inverter.
+// Solves lhs = A^(-1) rhs with CA-CG.
+// https://research.nvidia.com/sites/default/files/pubs/2016-04_S-Step-and-Communication-Avoiding/nvr-2016-003.pdf
+// Assumes the matrix is Hermitian (symmetric) positive definite.
+
+
+
+#ifndef QLINALG_INVERTER_CA_CG
+#define QLINALG_INVERTER_CA_CG
+
+#include <string>
+#include <sstream>
+#include <complex>
+
+using std::complex;
+using std::stringstream;
+
+
+#ifndef QLINALG_FCN_POINTER
+#define QLINALG_FCN_POINTER
+typedef void (*matrix_op_real)(double*,double*,void*);
+typedef void (*matrix_op_cplx)(complex<double>*,complex<double>*,void*);
+#endif
+
+#include "inverter_struct.h"
+#include "../verbosity/verbosity.h"
+
+#ifndef QLINALG_EIGEN_MATRIX
+#define QLINALG_EIGEN_MATRIX
+typedef Matrix<double, Dynamic, Dynamic, ColMajor> rSquareMatrix;
+typedef Matrix<double, Dynamic, 1> rVector;
+#endif
+
+inversion_info minv_vector_ca_cg(double  *phi, double  *phi0, int size, int max_iter, double res, int s, void (*matrix_vector)(double*,double*,void*), void* extra_info, inversion_verbose_struct* verbosity = 0);
+inversion_info minv_vector_ca_cg(complex<double>  *phi, complex<double>  *phi0, int size, int max_iter, double res, int s, void (*matrix_vector)(complex<double>*,complex<double>*,void*), void* extra_info, inversion_verbose_struct* verbosity = 0);
+
+// Solves lhs = A^(-1) with CG(m), where m is restart_freq. 
+inversion_info minv_vector_ca_cg_restart(double  *phi, double  *phi0, int size, int max_iter, double res, int s, int restart_freq, void (*matrix_vector)(double*,double*,void*), void* extra_info, inversion_verbose_struct* verbosity = 0);
+inversion_info minv_vector_ca_cg_restart(complex<double>  *phi, complex<double>  *phi0, int size, int max_iter, int s, double res, int restart_freq, void (*matrix_vector)(complex<double>*,complex<double>*,void*), void* extra_info, inversion_verbose_struct* verbosity = 0);
+
+inversion_info minv_vector_ca_cg(double  *phi, double  *phi0, int size, int max_iter, double eps, int s, void (*matrix_vector)(double*,double*,void*), void* extra_info, inversion_verbose_struct* verb)
+{
+
+  int k;
+  // Initialize vectors.
+  double *r, *p, *Ap;
+  double alpha, beta, rsq, rsqNew, bsqrt, truersq;
+  inversion_info invif;
+
+  // Allocate memory.
+  r = allocate_vector<double>(size);
+  p = allocate_vector<double>(size);
+  Ap = allocate_vector<double>(size);
+
+  // Initialize values.
+  rsq = 0.0; rsqNew = 0.0; bsqrt = 0.0; truersq = 0.0; k=0;
+
+  // Zero vectors;
+  zero_vector(r, size); 
+  zero_vector(p, size); zero_vector(Ap, size);
+  
+  // Find norm of rhs.
+  bsqrt = sqrt(norm2sq(phi0, size));
+  
+  // 1. Compute r = b - Ax using p as a temporary vector. 
+  (*matrix_vector)(p, phi, extra_info); invif.ops_count++;
+  cxpayz(phi0, -1.0, p, r, size);
+  
+  // 2. p_0 = r_0.
+  copy_vector(p, r, size);
+  
+  // Compute Ap.
+  zero_vector(Ap, size);
+  (*matrix_vector)(Ap, p, extra_info); invif.ops_count++;
+  
+  // Compute rsq.
+  rsq = norm2sq(r, size);
+
+  // iterate till convergence
+  for(k = 0; k< max_iter; k++) {
+    
+    // alpha = <r, r>/<p, Ap>
+    alpha = rsq/re_dot(p, Ap, size);
+
+    // phi += alpha*p
+    caxpy(alpha, p, phi, size);
+    
+    // r -= alpha*Ap
+    caxpy(-alpha, Ap, r, size);
+    
+    // Exit if new residual is small enough
+    rsqNew = norm2sq(r, size);
+    
+    print_verbosity_resid(verb, "CA-CG", k+1, invif.ops_count, sqrt(rsqNew)/bsqrt); 
+
+    if (sqrt(rsqNew) < eps*bsqrt || k == max_iter-1) {
+      //        printf("Final rsq = %g\n", rsqNew);
+      break;
+    }
+  
+    // Update vec using new residual
+    beta = rsqNew / rsq;
+    rsq = rsqNew; 
+    
+    // p = r + beta*p
+    cxpay(r, beta, p, size);
+    
+    // Compute the new Ap.
+    zero_vector(Ap, size);
+    (*matrix_vector)(Ap, p, extra_info); invif.ops_count++;
+  } 
+    
+  if(k == max_iter-1) {
+    invif.success = false;
+  }
+  else
+  {
+     invif.success = true;
+  }
+  k++;
+  
+  (*matrix_vector)(Ap,phi,extra_info); invif.ops_count++;
+  truersq = diffnorm2sq(Ap, phi0, size);
+  
+  // Free all the things!
+  deallocate_vector(&r);
+  deallocate_vector(&p);
+  deallocate_vector(&Ap);
+
+  print_verbosity_summary(verb, "CA-CG", invif.success, k, invif.ops_count, sqrt(truersq)/bsqrt);
+  
+  invif.resSq = truersq;
+  invif.iter = k;
+  invif.name = "CA-CG";
+  return invif; // Convergence 
+} 
+
+// Performs CA-CG(restart_freq) with restarts when restart_freq is hit.
+// This may be sloppy, but it works.
+inversion_info minv_vector_ca_cg_restart(double  *phi, double  *phi0, int size, int max_iter, double res, int s, int restart_freq, void (*matrix_vector)(double*,double*,void*), void* extra_info, inversion_verbose_struct* verb)
+{
+  int iter; // counts total number of iterations.
+  int ops_count; 
+  inversion_info invif;
+  double bsqrt = sqrt(norm2sq(phi0, size));
+  
+  inversion_verbose_struct verb_rest;
+  shuffle_verbosity_restart(&verb_rest, verb);
+  
+  stringstream ss;
+  ss << "CA-CG(" << restart_freq << ")";
+  
+  iter = 0; ops_count = 0; 
+  do
+  {
+    invif = minv_vector_cg(phi, phi0, size, min(max_iter, restart_freq), res, matrix_vector, extra_info, &verb_rest);
+    iter += invif.iter;
+    ops_count += invif.ops_count; 
+    
+    print_verbosity_restart(verb, ss.str(), iter, ops_count, sqrt(invif.resSq)/bsqrt);
+  }
+  while (iter < max_iter && invif.success == false && sqrt(invif.resSq)/bsqrt > res);
+  
+  invif.iter = iter; invif.ops_count = ops_count; 
+  
+  print_verbosity_summary(verb, ss.str(), invif.success, iter, invif.ops_count, sqrt(invif.resSq)/bsqrt);
+  
+  invif.name = ss.str();
+  // invif.resSq is good.
+  if (sqrt(invif.resSq)/bsqrt > res)
+  {
+    invif.success = false;
+  }
+  else
+  {
+    invif.success = true;
+  }
+  
+  return invif;
+}
+
+/*
+inversion_info minv_vector_ca_cg(complex<double>  *phi, complex<double>  *phi0, int size, int max_iter, double eps, int s, void (*matrix_vector)(complex<double>*,complex<double>*,void*), void* extra_info, inversion_verbose_struct* verb)
+{
+
+  int k;
+  // Initialize vectors.
+  complex<double> *r, *p, *q;
+  double alpha; 
+  double beta;
+  complex<double> denom;
+  double rsq, rsqNew, bsqrt, truersq;
+  inversion_info invif;
+
+  // Allocate memory.
+  r = allocate_vector<complex<double>>(size);
+  p = allocate_vector<complex<double>>(size);
+  q = allocate_vector<complex<double>>(size);
+
+  // Initialize values.
+  rsq = 0.0; rsqNew = 0.0; bsqrt = 0.0; truersq = 0.0; k=0;
+
+  // Zero vectors;
+  zero_vector(r, size); 
+  zero_vector(p, size); zero_vector(q, size);
+  
+  // Find norm of rhs.
+  bsqrt = sqrt(norm2sq(phi0, size));
+  
+  // 2. Compute r = b - Ax
+  (*matrix_vector)(p, phi, extra_info); invif.ops_count++;
+  cxpayz(phi0, -1.0, p, r, size);
+  rsq = norm2sq(r, size);
+
+  // 3: for i = 0, 1, ... until convergence do
+  for(k = 0; k< max_iter; k++) {
+
+    // 4: if i == 0 then
+    if (k == 0) {
+      // 5: Set p_i = r_i
+      copy_vector(p, r, size);
+
+    } else { // 6: else...
+
+      // 7: Compute beta = r2/rold2
+      beta = rsqNew / rsq;
+      rsq = rsqNew;
+
+      // 8: Compute p = r + beta p
+      cxpay(r, beta, p, size);
+
+    } // 9: end if
+
+    // 10: Compute q = Ap
+    (*matrix_vector)(q, p, extra_info); invif.ops_count++;
+
+    // 11: Compute alpha = r2/r_dot_q
+    alpha = rsq/re_dot(r, q, size); // used to be p Ap? I guess there's an ortho arg here
+    
+    // 12: Compute phi += alpha p
+    caxpy(alpha, p, phi, size);
+    
+    // 13: Compute r -= alpha q
+    caxpy(-alpha, q, r, size);
+    
+    // 14: if r2_new/r2 < tol then stop
+    rsqNew = norm2sq(r, size);
+    print_verbosity_resid(verb, "CA-CG", k+1, invif.ops_count, sqrt(rsqNew)/bsqrt);
+    if (sqrt(rsqNew) < eps*bsqrt || k == max_iter - 1) {
+      break;
+    }
+    
+  } 
+    
+  if(k == max_iter-1) {
+    invif.success = false;
+  }
+  else
+  {
+     invif.success = true;
+  }
+	
+  k++; 
+  
+  (*matrix_vector)(q,phi,extra_info); invif.ops_count++;
+  truersq = diffnorm2sq(q, phi0, size);
+  
+  // Free all the things!
+  deallocate_vector(&r);
+  deallocate_vector(&p);
+  deallocate_vector(&q);
+
+  
+  print_verbosity_summary(verb, "CA-CG", invif.success, k, invif.ops_count, sqrt(truersq)/bsqrt);
+  
+  
+  invif.resSq = truersq;
+  invif.iter = k;
+  invif.name = "CA-CG";
+  return invif; // Convergence 
+}
+*/
+
+inversion_info minv_vector_ca_cg(complex<double>  *phi, complex<double>  *phi0, int size, int max_iter, double eps, int s, void (*matrix_vector)(complex<double>*,complex<double>*,void*), void* extra_info, inversion_verbose_struct* verb)
+{
+
+  int k;
+
+  // Initialize vectors.
+  complex<double> *T[s+1];
+  complex<double> *R[s];
+  complex<double> *Q[s];
+  complex<double> *P[s], *Ptmp[s];
+  rVector a(s), g(s);
+  rSquareMatrix B(s,s), C(s,s), W(s,s);
+  //rSquareMatrix Rsq(s,s), RsqNew(s,s);
+
+  double rsq, bsqrt, truersq;
+  inversion_info invif;
+
+  // Allocate memory.
+  for (int i = 0; i <= s; i++) {
+    T[i] = allocate_vector<complex<double>>(size);
+    R[i] = T[i];
+    if (i > 0) { Q[i-1] = T[i]; }
+    zero_vector(T[i], size);
+  }
+
+  for (int i = 0; i < s; i++) {
+    P[i] = allocate_vector<complex<double>>(size);
+    Ptmp[i] = allocate_vector<complex<double>>(size);
+    zero_vector(P[i], size);
+    zero_vector(Ptmp[i], size);
+  }
+
+  // Initialize values.
+  rsq = 0.0; bsqrt = 0.0; truersq = 0.0; k=0;
+  
+  // Find norm of rhs.
+  bsqrt = sqrt(norm2sq(phi0, size));
+  
+  // 2. Compute r = b - Ax
+  (*matrix_vector)(P[0], phi, extra_info); invif.ops_count++;
+  cxpayz(phi0, -1.0, P[0], R[0], size);
+
+  // 3: for i = 0, s, 2s ... until convergence do
+  for(k = 0; k< max_iter; k+=s) {
+
+    // 4: Compute T = [r_k, ..., A^s r_k]
+    for (int i = 0; i < s; i++) {
+      (*matrix_vector)(T[i+1], T[i], extra_info); invif.ops_count++;
+    }
+
+    // 5: Let R_i = [r_k, Ar_k, ..., A^{s-1} r_k] 
+    // no op: taken care of by aliasing.
+
+    // 6: Let Q_i = [Ar_k, A^2 r_k, ..., A^s r_k]
+    // no op: taken care of by aliasing.
+
+    // Edit: get the R outer product matrix.
+    /*if (k != 0) {
+      Rsq = RsqNew; 
+    }
+    for (int i = 0; i < s; i++) {
+      for (int j = 0; j < s; j++) {
+        RsqNew(i,j) = re_dot(R[i], R[j],size);
+      }
+    }*/
+
+    // 7: if i == 0 then
+    if (k == 0) {
+      // 8: Set P = R
+      for (int i = 0; i < s; i++) {
+        copy_vector(P[i], R[i], size);
+      }
+    } else { // 9: else
+
+      // 10: Compute C_i = -Q_i^dag P [may need to switch?]
+      for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+          C(i,j) = -re_dot(P[i], Q[j],size);
+        }
+      }
+
+      // 11: Solve W_{i-1} B_i = C_i
+      B = W.fullPivLu().solve(C); //Rsq.fullPivLu().solve(RsqNew); 
+      
+      // 12: Compute P = R + P B
+      for (int i = 0; i < s; i++) {
+        copy_vector(Ptmp[i], R[i], size);
+      }
+      for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+          caxpy(double(B(i,j)), P[i], Ptmp[j], size);
+        }
+      }
+      for (int i = 0; i < s; i++) {
+        copy_vector(P[i], Ptmp[i], size);
+      }
+ 
+      // 13: end if
+    }
+
+    // 14: Compute W = Q^dag P;
+    for (int i = 0; i < s; i++) {
+      for (int j = 0; j < s; j++) {
+        W(i,j) = re_dot(P[i],Q[j],size);
+      }
+    }
+
+    // 15: Compute g = P^dag r_i
+    for (int i = 0; i < s; i++) {
+      g(i) = re_dot(P[i], R[0], size);
+    }
+
+    // 16: Solve W a = g
+    a = W.fullPivLu().solve(g);
+
+    // 17: Compute x_{k+s} = P_i a_i
+    for (int i = 0; i < s; i++) {
+      caxpy(a(i), P[i], phi, size);
+    }
+
+    // 18: Compute r_{k+s} = b - A x_{k+s}
+    zero_vector(Ptmp[0], size);
+    (*matrix_vector)(Ptmp[0], phi, extra_info); invif.ops_count++;
+    cxpayz(phi0, -1.0, Ptmp[0], R[0], size);
+
+    // 19: Check convergence
+    rsq = norm2sq(R[0], size);
+    print_verbosity_resid(verb, "CA-CG", k+s, invif.ops_count, sqrt(rsq)/bsqrt);
+    if (sqrt(rsq) < eps*bsqrt || k == max_iter - s) {
+      break;
+    }
+
+  } 
+    
+  if(k == max_iter-s) {
+    invif.success = false;
+  }
+  else
+  {
+     invif.success = true;
+  }
+  
+  k++; 
+  
+  zero_vector(Ptmp[0], size);
+  (*matrix_vector)(Ptmp[0],phi,extra_info); invif.ops_count++;
+  truersq = diffnorm2sq(Ptmp[0], phi0, size);
+  
+  // Free all the things!
+  for (int i = 0; i <= s; i++) {
+    deallocate_vector(&T[i]);
+    if (i < s) {
+      deallocate_vector(&P[i]);
+      deallocate_vector(&Ptmp[i]);
+    }
+  }
+
+  
+  print_verbosity_summary(verb, "CA-CG", invif.success, k, invif.ops_count, sqrt(truersq)/bsqrt);
+  
+  
+  invif.resSq = truersq;
+  invif.iter = k+s-1;
+  invif.name = "CA-CG";
+  return invif; // Convergence 
+}
+
+
+// Performs CG(restart_freq) with restarts when restart_freq is hit.
+// This may be sloppy, but it works.
+inversion_info minv_vector_ca_cg_restart(complex<double>  *phi, complex<double>  *phi0, int size, int max_iter, double res, int s, int restart_freq, void (*matrix_vector)(complex<double>*,complex<double>*,void*), void* extra_info, inversion_verbose_struct* verb)
+{
+  int iter; // counts total number of iterations.
+  int ops_count; 
+  inversion_info invif;
+  double bsqrt = sqrt(norm2sq(phi0, size));
+  
+  inversion_verbose_struct verb_rest;
+  shuffle_verbosity_restart(&verb_rest, verb);
+  
+  stringstream ss;
+  ss << "CA-CG(" << restart_freq << ")";
+  
+  iter = 0; ops_count = 0; 
+  do
+  {
+    invif = minv_vector_cg(phi, phi0, size, min(max_iter, restart_freq), res, matrix_vector, extra_info, &verb_rest);
+    iter += invif.iter;
+    ops_count += invif.ops_count; 
+    
+    print_verbosity_restart(verb, ss.str(), iter, ops_count, sqrt(invif.resSq)/bsqrt);
+  }
+  while (iter < max_iter && invif.success == false && sqrt(invif.resSq)/bsqrt > res);
+  
+  invif.iter = iter; invif.ops_count = ops_count; 
+  
+  print_verbosity_summary(verb, ss.str(), invif.success, iter, invif.ops_count, sqrt(invif.resSq)/bsqrt);
+  
+  invif.name = ss.str();
+  // invif.resSq is good.
+  if (sqrt(invif.resSq)/bsqrt > res)
+  {
+    invif.success = false;
+  }
+  else
+  {
+    invif.success = true;
+  }
+  
+  return invif;
+}
+
+
+
+#endif 
